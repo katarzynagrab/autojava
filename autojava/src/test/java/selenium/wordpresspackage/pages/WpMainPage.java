4c1ca6cb7581616f166d5b51e6deeff311aada26
katarzynagrab <grabowska.katarzyna@wp.pl>
2018-12-13 16:52:55 +0100

    Wordpresspage

--- autojava/src/test/java/selenium/wordpresspackage/pages/WpMainPage.java
+++ autojava/src/test/java/selenium/wordpresspackage/pages/WpMainPage.java
@@ -1,6 +1,8 @@
 package selenium.wordpresspackage.pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class WpMainPage {
     private final WebDriver driver;
@@ -8,10 +10,13 @@ public class WpMainPage {
     public WpMainPage(WebDriver driver) {
         this.driver = driver;
         //Todo: here you open wordpress page
+        this.driver.get("https://automatyzacja.benedykt.net/");
     }
 
     public WpNotePage openLatestNote() {
         //Todo: here you find and open latest note (first note)
+        WebElement LinkBox = driver.findElement(By.cssSelector(".entry-title > a"));
+        LinkBox.click();
         return new WpNotePage(driver);
     }
 }
--- autojava/src/test/java/selenium/wordpresspackage/pages/WpNotePage.java
+++ autojava/src/test/java/selenium/wordpresspackage/pages/WpNotePage.java
@@ -1,6 +1,8 @@
 package selenium.wordpresspackage.pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class WpNotePage {
     private final WebDriver driver;
@@ -10,7 +12,9 @@ public class WpNotePage {
     }
 
     public WpNotePage addComment(String comment, String author, String email) {
-        //here you publish comment
+        //Todo: here you publish comment
+        WebElement SubmitBox = driver.findElement(By.cssSelector("#submit"));
+        SubmitBox.click();
         return new WpNotePage(driver);
     }
 
